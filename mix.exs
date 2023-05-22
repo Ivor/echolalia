61cc212de49e4132352d99c69ab058742b714a9f
Ivor Paul <ivor.paul@lifeq.com>
2023-05-22 18:39:28 +0200

    Setup Eliver

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Echolalia.MixProject do
   def project do
     [
       app: :echolalia,
-      version: "0.1.0",
+      version: String.trim(File.read!("VERSION")),
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
